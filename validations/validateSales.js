dc3c644d2a14f7670cb8e2f197992246d817d9d7
Marcos Albuquerque <mpdsa20@gmail.com>
2021-10-27 23:01:06 -0300

    Fix de logica de validaçao das vendas

--- validations/validateSales.js
+++ validations/validateSales.js
@@ -4,12 +4,18 @@ const sendStatusError = require('./sendStatusError');
 const MESSAGE_ERROR1 = 'Wrong product ID or invalid quantity';
 
 function sales(req, res, next) {
-  const errQuantity = req.body.some((ele) => ele.quantity <= 0);
-  const errTypeString = req.body.some((ele) => (typeof (ele.quantity) === 'string'));
+  const errQuantity = req.body.some(({ quantity }) => quantity < 1);
+  const errTypeString = req.body.some(({ quantity }) => typeof (quantity) === 'string');
 
-  if (!(errQuantity && errTypeString)) {
+  if (errQuantity) {
     return sendStatusError(StatusCodes.UNPROCESSABLE_ENTITY, MESSAGE_ERROR1, res);
-  } next();
+  }
+  
+  if (errTypeString) {
+    return sendStatusError(StatusCodes.UNPROCESSABLE_ENTITY, MESSAGE_ERROR1, res);
+  }
+
+  next();
 }
 
 module.exports = sales;
